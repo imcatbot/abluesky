58dc5ab7f62f1d3e8da063fbe6a7dbf9538577b0
catbot <you@example.com>
2012-12-16 21:09:34 +0800

    trival changes

--- build-os.sh
+++ build-os.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/bash 
 
 # Coded by Jiang Wei
 # Build a Live CD against stander Debian Live CD
@@ -7,47 +7,26 @@
 CURRENT_DIR=`pwd`
 BUILD_DIR=build
 
-# Check ISO in cache directory, download from website of debian if not exist
-isos=`find cache -maxdepth 1 -iname "debian*.iso"`
-ORIG_ISO=
-
-for iso in ${isos}
-do
-    if [ -e "${iso}" ]
-    then
-	ORIG_ISO=${CURRENT_DIR}/${iso}
-	break
-    fi
-done
-
-echo $ORIG_ISO
-
 # Create build directory 
-mkdir -p $BUILD_DIR
-mkdir -p $BUILD_DIR/mnt
-mkdir -p $BUILD_DIR/mnt_fs
-mkdir -p $BUILD_DIR/chroot
+mkdir -p $BUILD_DIR/newchroot
 mkdir -p $BUILD_DIR/newcd
+mkdir -p $BUILD_DIR/newcd/live
 
-mount -o loop $ORIG_ISO $BUILD_DIR/mnt
-ORIG_FILESYSTEM=$BUILD_DIR/mnt/live/filesystem.squashfs
-mount -o loop $ORIG_FILESYSTEM $BUILD_DIR/mnt_fs
+# Copy filesystem to newchroot for building
+(cd $BUILD_DIR/chroot && tar -cf - --exclude=var/cache/apt/archives/* --exclude=usr/share/doc/* . )| tar -x -C $BUILD_DIR/newchroot
 
-# Copy all files from CD beside filesystem
-(cd $BUILD_DIR/mnt && tar -cf - --exclude=live/filesystem.squashfs . )| tar -x -C $BUILD_DIR/newcd
-
-# Copy filesystem to chroot for building
-(cd $BUILD_DIR/mnt_fs && tar -cf - . )| tar -x -C $BUILD_DIR/chroot
-
-#umount iso
-umount $BUILD_DIR/mnt_fs
-umount $BUILD_DIR/mnt
+# Clean up filesystem
 
 # Re-package filesystem
-mksquashfs $BUILD_DIR/chroot $BUILD_DIR/newcd/live/filesystem.squashfs
+mksquashfs $BUILD_DIR/newchroot $BUILD_DIR/newcd/live/filesystem.squashfs
+
+rm -rf $BUILD_DIR/newchroot
 
 # Copy 3rdparty
 cp -a 3rdparty/isolinux $BUILD_DIR/newcd/
+
+cp -a $BUILD_DIR/orig_cd/live/initrd* $BUILD_DIR/newcd/live
+cp -a $BUILD_DIR/orig_cd/live/vmlinuz* $BUILD_DIR/newcd/live
 
 # Build iso
 
@@ -56,10 +35,7 @@ genisoimage -l -o abluesky.iso  \
 -no-emul-boot -boot-load-size 4 -boot-info-table \
 build/newcd/
 
-if [ "${CLEAN_FLAG}" = "True" ]
-then
-    rm -rf $BUILD_DIR
-fi
+rm -rf build/newcd/
 
 echo Done
 
