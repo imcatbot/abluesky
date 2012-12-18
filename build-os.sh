3875bb7dc24ee0bb93a2d131e73b415cc1759485
catbot <you@example.com>
2012-12-18 22:56:51 +0800

    add gfxboot-message

--- build-os.sh
+++ build-os.sh
@@ -7,10 +7,15 @@
 CURRENT_DIR=`pwd`
 BUILD_DIR=build
 
+GFXBOOT_FONT=/home/catbot/develop/abluesky/gfxboot/gfxboot-font
+FONT_PATH=/usr/share/fonts/xpfonts
+FONT_NAME=simhei
+
 # Create build directory 
 mkdir -p $BUILD_DIR/newchroot
 mkdir -p $BUILD_DIR/newcd
 mkdir -p $BUILD_DIR/newcd/live
+mkdir -p $BUILD_DIR/tmp
 
 # Copy filesystem to newchroot for building
 (cd $BUILD_DIR/chroot && tar -cf - --exclude=var/cache/apt/archives/* --exclude=usr/share/doc/* . )| tar -x -C $BUILD_DIR/newchroot
@@ -24,6 +29,15 @@ rm -rf $BUILD_DIR/newchroot
 
 # Copy 3rdparty
 cp -a 3rdparty/isolinux $BUILD_DIR/newcd/
+
+# Create gfxboot message
+cp -a 3rdparty/gfxboot-message/ $BUILD_DIR/
+(cd $BUILD_DIR/gfxboot-message && cat addon.txt anscii.txt translations.zh_CN zh_CN.hlp > chs.txt)
+(cd $BUILD_DIR/gfxboot-message && ${GFXBOOT_FONT} -v -t chs.txt -p ${FONT_PATH} -f ${FONT_NAME} 16x16.fnt >16x16.fnt.log)
+(cd $BUILD_DIR/gfxboot-message && find .|cpio -o > $BUILD_DIR/newcd/isolinux/message)
+
+# Clean gfxboot message
+rm -rf $BUILD_DIR/gfxboot-message
 
 cp -a $BUILD_DIR/orig_cd/live/initrd* $BUILD_DIR/newcd/live
 cp -a $BUILD_DIR/orig_cd/live/vmlinuz* $BUILD_DIR/newcd/live
